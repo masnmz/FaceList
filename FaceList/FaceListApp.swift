c928f8ef251610426be6dba0949be5a055234d45
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-07-02 13:03:16 +0100

    Initial Commit

---
+++ FaceList/FaceListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceListApp.swift
+//  FaceList
+//
+//  Created by Mehmet Alp Sönmez on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
